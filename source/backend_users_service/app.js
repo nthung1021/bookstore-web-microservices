81a68500074faf88712b4a4700d493ad2e82d579
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-06-05 23:33:48 +0700

    Fix the CORS Error

--- source/backend_users_service/app.js
+++ source/backend_users_service/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 app.use(express.json());
 app.use('/api/user', userRoutes);
+app.use(cors({
+  origin: process.env.USER_API_URL
+}));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`User service on ${PORT}`));

7866db2bd38c073e33b1de4e71a190128765d5c0
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-06-06 21:44:23 +0700

    Add catalog service API endpoint

--- source/frontend/env.js
+++ source/frontend/env.js
@@ -1,3 +1,4 @@
 window.env = {
-    USER_API_URL : 'https://bookstore-microservices-user-sevice.onrender.com'
+    USER_API_URL : 'https://bookstore-microservices-user-sevice.onrender.com',
+    CATALOG_API_URL: 'https://bookstore-microservices-catalog-service.onrender.com'
 };

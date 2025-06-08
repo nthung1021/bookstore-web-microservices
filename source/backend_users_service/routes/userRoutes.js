880a8aa09ae5e43a63edf617cf62dc15772007ab
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-06-09 01:24:20 +0700

    Fix "cannot fetch user data" error

--- source/backend_users_service/routes/userRoutes.js
+++ source/backend_users_service/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getUserById } = require('../controllers/userController');
+const { register, login, getUserById } = require('../controllers/userControllers');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/:id', getUserById);
+router.get('/:id', getUserById);
 
 module.exports = router;

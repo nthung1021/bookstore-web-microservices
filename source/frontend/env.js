192199b837a894e696d5951e1540d4fa7359fd19
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-06-08 23:52:10 +0700

    Add Order button to purchase books, create Cart and Order pages

--- source/frontend/env.js
+++ source/frontend/env.js
@@ -1,4 +1,5 @@
 window.env = {
     USER_API_URL : 'https://bookstore-microservices-user-sevice.onrender.com',
-    CATALOG_API_URL: 'https://bookstore-microservices-catalog-service.onrender.com'
+    CATALOG_API_URL: 'https://bookstore-microservices-catalog-service.onrender.com',
+    ORDER_API_URL: 'https://bookstore-microservices-order-service.onrender.com'
 };

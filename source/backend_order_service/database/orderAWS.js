84bc594d4d0e00d82c91c37c58370fbd001ca242
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-06-11 14:58:36 +0700

    Fix error no pg_hba.conf entry for host

--- source/backend_order_service/database/orderAWS.js
+++ source/backend_order_service/database/orderAWS.js
@@ -6,6 +6,9 @@ const pool = new Pool({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.query('SELECT NOW()', (err, result) => {

324ebfaa4006a8806bfe6994788c1bff6a5af3b4
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-06-16 17:00:23 +0700

    Modify frontend to call API services through API Gateway

--- source/frontend/env.js
+++ source/frontend/env.js
@@ -1,5 +1,3 @@
 window.env = {
-    USER_API_URL : 'https://bookstore-microservices-user-sevice.onrender.com',
-    CATALOG_API_URL: 'https://bookstore-microservices-catalog-service.onrender.com',
-    ORDER_API_URL: 'https://bookstore-microservices-order-service.onrender.com'
+    API_URL : 'https://bookstore-microservices-api-gateway.onrender.com'
 };
